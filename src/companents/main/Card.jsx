472e57f94dbf2e4d3d2965d7c6ffc920a030c6e3
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-06-28 14:36:48 +0300

    it was updated

--- src/companents/main/Card.jsx
+++ src/companents/main/Card.jsx
@@ -2,7 +2,7 @@
 
 const Card = (data) => {
     console.log("ne gliyor", data);
-    const {id, title, image, desc} = data;
+    const {title, image, desc} = data;
   return (
     <div className="cards">
         <div className="title">
