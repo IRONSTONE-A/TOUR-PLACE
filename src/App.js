2fc1130e080df0db29921fe5a75a6206a9faa6dd
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-08-03 13:45:54 +0300

    updated and added the gif

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import "./App.scss"
-import Header from "./companents/header/Header";
-import Main from "./companents/main/Main";
-import Navbar from "./companents/navbar/Navbar";
+import './App.scss'
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
 
-
-const App = () => {
-
+const App = () =>{
   return(
     <div className="App">
       <Navbar/>
       <Header/>
       <Main/>
-      
-
-      
     </div>
-
-
   )
 }
 
